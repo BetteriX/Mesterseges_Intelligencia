4ebfa7387c3c1855aaaa52146b9e1d4bf93fc069
BetteriX <szabozsolt430@gmail.com>
2026-03-09 13:53:57 +0100

    4HF

--- 4/matrix_kiralyno.py
+++ 4/matrix_kiralyno.py
@@ -6,15 +6,14 @@ import numpy as np
 
 class Kiralynok:
     def __init__(self, ke, c):
-        self.kezdo = ke  # (tábla, kezdő sor)
-        self.cel = c  # utolsó sor index
-        self.n = len(ke[0])  # tábla mérete (8)
+        self.kezdo = ke
+        self.cel = c
+        self.n = len(ke[0])
 
     def celteszt(self, a):
-        # ha az aktuális sor = cél, minden királynő elhelyezve
         return a[1] == self.cel
 
-    def rakovetkezo(self, t):  # ← itt az osztály részeként
+    def rakovetkezo(self, t):
         gyerekek = []
         s = t[1]
         a = t[0].copy()
@@ -22,13 +21,11 @@ class Kiralynok:
         for i in range(self.n):
             elofeltetel = True
 
-            # oszlop ellenőrzés
-            for row in range(s - 1):  # s-1, mert kezdő sor 1
+            for row in range(s - 1):
                 if a[row, i] == 1:
                     elofeltetel = False
                     break
 
-            # diagonális ellenőrzés abs segítségével
             if elofeltetel:
                 for row in range(s - 1):
                     for col in range(self.n):
@@ -39,16 +36,16 @@ class Kiralynok:
                         break
 
             if elofeltetel:
-                uj_allpot = a.copy()
-                uj_allpot[s - 1, i] = 1
-                gyerekek.append((uj_allpot, s + 1))
+                uj_allapot = a.copy()
+                uj_allapot[s - 1, i] = 1
+                gyerekek.append((uj_allapot, s + 1))
 
         return gyerekek
 
 
 def main():
     tabla = np.zeros((8, 8), dtype=int)
-    feladat = Kiralynok((tabla, 1), 9)  # kezdő sor 1, cél sor 9
+    feladat = Kiralynok((tabla, 1), 9)
 
     allapotok = [feladat.kezdo]
 
